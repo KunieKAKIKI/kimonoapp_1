2d3cb6479c17b9a9537f3517a0b2e544e39cc924
kakiki <kakikunie@icloud.com>
2018-02-19 23:11:09 +0900

    item作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219140743) do
+ActiveRecord::Schema.define(version: 20180219141027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "coordinates", force: :cascade do |t|
+    t.string "title"
+    t.text "image"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "image"
     t.text "content"
