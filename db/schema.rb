bc04ffdb81ab06c1c2cb9ae83eda9df7304cc798
kakiki <kakikunie@icloud.com>
2018-02-20 00:58:21 +0900

    itemにcategory_id追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219155404) do
+ActiveRecord::Schema.define(version: 20180219155624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20180219155404) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -65,5 +67,6 @@ ActiveRecord::Schema.define(version: 20180219155404) do
   end
 
   add_foreign_key "coordinates", "users"
+  add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
 end
