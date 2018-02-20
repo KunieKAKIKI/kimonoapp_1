b1271e449fab98824db259560881bf7925ef1e80
kakiki <kakikunie@icloud.com>
2018-02-21 02:43:28 +0900

    bootstrap

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -2,7 +2,7 @@ class ChoicesController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     choice = @item.choices.create(coordinate_id: params[:coordinate_id])
-    @choice = @item.choices.find_by(coordinate_id: @coordinate.id
+    @choice = @item.choices.find_by(coordinate_id: params[:coordinate_id])
     redirect_to choicelist_url
   end
 
