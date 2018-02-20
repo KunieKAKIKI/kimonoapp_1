f960de59b5885818486359ec87cf3f9247f86313
kakiki <kakikunie@icloud.com>
2018-02-20 20:01:57 +0900

    choicelist作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def choicelist
+    #@coordinate = Coordinate.find(params[:coordinate_id])
+    @user = current_user
+    @items = Item.where(user_id: @user.id)
+  end
+
+  def favoritelist
+    @user = current_user
+    @favorite_coordinates = @user.favorite_coordinates
+  end
+
 end
